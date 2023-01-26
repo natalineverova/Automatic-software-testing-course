542c91f331bca9a9a15ac28f9cdf2cce7cbd1a7b
Nataliia Nevierova <neverova.natali@gmail.com>
2023-01-26 21:14:39 +0200

    Optimization

---
+++ pytest/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Nataliia"
+        self.second_name = "Nevierova"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+    yield user
+    user.remove()
